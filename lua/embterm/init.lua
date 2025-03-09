563955877fd2ba80290030d2c8f724d60944284a
kennytheeggman <kenny.kh.huang@gmail.com>
2025-03-08 23:43:27 -0500

    restore original functionality

--- lua/embterm/init.lua
+++ lua/embterm/init.lua
@@ -2,20 +2,28 @@ local win = require('lua.embterm.window')
 local obj
 
 
---/// write a lua function that says hello world
-
-
 vim.api.nvim_create_user_command('EmbTermOpen', function()
 	local path = debug.getinfo(1).source:sub(2, -9)
 	obj = win.new({
 		cmd1 = function(O)
-			local start = vim.api.nvim_buf_get_mark(O.pbufnr, "<")
-			local last = vim.api.nvim_buf_get_mark(O.pbufnr, ">")
-			local command = "python3 "..path..'main.py .tmp.diff '..O.path..' '..start[1]..' '..last[1]
-			vim.schedule(function() print(command) end)
-			vim.fn.termopen(command)
-			vim.cmd("resize 3")
-			vim.cmd("set nonumber")
+		    -- Get the start and last marks of the current buffer
+		    local start = vim.api.nvim_buf_get_mark(O.pbufnr, "<")
+		    local last = vim.api.nvim_buf_get_mark(O.pbufnr, ">")
+
+		    -- Construct the command to run the Python script
+		    local command = "python3 "..path..'main.py .tmp.diff '..O.path..' '..start[1]..' '..last[1]
+
+		    -- Schedule the print of the command after a short delay
+		    vim.schedule(function() print(command) end)
+
+		    -- Open the terminal and execute the command
+		    vim.fn.termopen(command)
+
+		    -- Resize the window to 3 lines
+		    vim.cmd("resize 3")
+
+		    -- Disable line numbers
+		    vim.cmd("set nonumber")
 		end,
 		cmd2 = function(O)
 			vim.cmd("diffthis")
